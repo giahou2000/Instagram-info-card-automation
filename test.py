32a4b0976fe5f7be4d5b29028dbbe89f93af7965
Christos Giachoudis <giachris2000@gmail.com>
2025-03-08 15:56:30 +0100

    blur photo success

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
+sigma = 20.0
+
 filename = os.path.join(ski.data_dir, 'IMG_2515.JPG')
 img = ski.io.imread(filename)
 
@@ -15,8 +17,9 @@ print(img.min(), img.max())
 print("mean")
 print(img.mean())
 
-blur_img = ski.filters.gaussian(img, sigma=0.4)
-# ski.filters.gaussian(img, sigma=1, mode='reflect')
+# blur_img = ski.filters.gaussian(img, sigma=0.4)
+# blur_img = ski.filters.gaussian(img, sigma=1, mode='wrap')
+blur_img = ski.filters.gaussian(img, sigma=(sigma, sigma), truncate=3.5, channel_axis=-1)
 
 ski.io.imshow(blur_img)
 plt.show()
