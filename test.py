c1b38cb440297353e356126e3026df8a710ac45d
Christos Giachoudis <giachris2000@gmail.com>
2025-03-13 20:15:03 +0100

    Use specific metadata

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+
 # 2. Read the metadata
 
 # path to the image or video
@@ -29,7 +30,37 @@ for tagid in exifdata:
     # printing the final result
     print(f"{tagname:25}: {value}")
 
-# 3. Gather all the useful metadata
+# 3. Gather specific useful metadata in a dictionary
+metadata = {}
+
+# Define the specific tags we are interested in
+specific_tags = {
+    'DateTime': 'Date and Time',
+    'Model': 'Camera Model',
+    'LensModel': 'Lens Model',
+    'FNumber': 'Aperture',
+    'ExposureTime': 'Exposure Time',
+    'ISOSpeedRatings': 'ISO',
+    'FocalLength': 'Focal Length',
+    'Software': 'Software'
+}
+
+# looping through all the tags present in exifdata
+for tagid in exifdata:
+     
+    # getting the tag name instead of tag id
+    tagname = TAGS.get(tagid, tagid)
+ 
+    # if the tagname is in our specific tags list, store it
+    if tagname in specific_tags:
+        # passing the tagid to get its respective value
+        value = exifdata.get(tagid)
+        # storing the tagname and value in the dictionary
+        metadata[specific_tags[tagname]] = value
+
+# printing the metadata dictionary
+print('Dictionary')
+print(metadata)
 
 # 4. Blur image
 
